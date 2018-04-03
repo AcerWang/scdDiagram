2c14cc6c7ca4a185222b7b6f56234cde6289622f
AcerWang <wxy910101@gmail.com>
2018-04-03 22:24:54 +0800

    Add files via upload

--- resolve.py
+++ resolve.py
@@ -1,5 +1,7 @@
+import db
 import xml.etree.ElementTree as ET
 import time
+
 
 start = time.clock()
 
@@ -8,11 +10,22 @@ root = tree.getroot()
 
 print(root.tag)
 total = 0
+insert_sql = "insert into ied values"
 for item in root.iter('{http://www.iec.ch/61850/2003/SCL}IED'):
     total += 1
     #print(item.find('title').text)
-    print(item.get('desc'))
-print("总共%d条数据！"%total)
+    name = item.get('name')
+    desc = item.get('desc')
+    Type = item.get('type')
+    manufacturer = item.get('manufacturer')
+    version = item.get('configVersion')
+    insert_sql += '(%d,"%s","%s","%s","%s","%s"),'%(1,name,desc,Type,manufacturer,version)
+insert_sql = insert_sql[:-1]+';'
+
+print(insert_sql[0:100])
+db.insert(insert_sql)
+#print("总共%d条数据！"%total)
+
 end = time.clock()
 
 print("程序总运行时间：",end-start,"s")
