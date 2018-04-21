7026fa8767c31c818d661999500415dac3887362
AcerWang <wxy910101@gmail.com>
2018-04-21 23:15:40 +0800

    Add files via upload

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import sqlite3
 def insert(sql,data):
 	res = 0
 	try:
-		conn = sqlite3.connect('HS220.db')
+		conn = sqlite3.connect('BZB.db')
 		cursor = conn.cursor()
 		cursor.executemany(sql,data)
 		res = cursor.rowcount
@@ -14,3 +14,21 @@ def insert(sql,data):
 	except Exception as e:
 		print(e)
 	print("插入数据%d条！"%res)
+
+def select(sql):
+	res = None
+	try:
+		conn = sqlite3.connect('STHB.db')
+		cursor = conn.cursor()
+		cursor.execute(sql)
+		res = cursor.fetchall()
+		cursor.close()
+		conn.close()
+	except Exception as e:
+		print(e)
+	if res is not None:
+		print('取得数据')
+		return res
+	else:
+		print("无可用数据")
+		return None
