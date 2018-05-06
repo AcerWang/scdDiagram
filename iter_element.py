2929de55fbc24a5c15c5a8a0385c8062476cf657
AcerWang <wxy910101@gmail.com>
2018-05-06 21:01:20 +0800

    Add files via upload

--- iter_element.py
+++ iter_element.py
@@ -3,7 +3,7 @@ import db
 import xml.etree.ElementTree as ET
 import time
 
-def resolve(scd_file,element,element_parent=None):
+def resolve(scd_file,element,element_parent):
     '''
         resolve IED from scd file,
         write IED data into database.
@@ -18,37 +18,33 @@ def resolve(scd_file,element,element_parent=None):
     fields = ','.join(['?' for e in tb_desc])
     insert_sql = "insert into %s values(%s)"%(element,fields)
     
-    if element_parent is None:
-        parent_tag = None
-    else:
-        parent_tag = '{http://www.iec.ch/61850/2003/SCL}'+element_parent
-    element_tag = '{http://www.iec.ch/61850/2003/SCL}'+element
+    ns = '{http://www.iec.ch/61850/2003/SCL}'
 
     for event,ele in iterObj:
         if event == 'start':
-            if parent_tag and ele.tag == parent_tag:
-                parent_no += 1
-            if ele.tag ==element_tag:
-                element_no += 1
+            if ele.tag == ns+element_parent:
+                parent_no += 1               
+        else:
+            if ele.tag != ns+element_parent:
+                continue
+            for e in ele.iterfind(ns+element):
                 attrs = []
+                element_no += 1
                 for field in tb_desc:
-                    if element_parent and field.startswith(str.lower(element_parent)):
+                    if field.startswith(str.lower(element_parent)):
                         attrs.append(field+"_"+str(parent_no))
                     elif field.startswith(str.lower(element)):
                         attrs.append(field+"_"+str(element_no))
                     else:
-                        attrs.append(ele.get(field))
+                        attrs.append(e.get(field))
                 data.append(tuple(attrs))
-        elif event == 'end':
             ele.clear()
-    
     db.insert(insert_sql,data)
-    
-    print("总共{%d}条数据！"%element_no)
+    print("总共{%d,%d}条数据！"%(parent_no,element_no))
 
 if __name__ == '__main__':
     start = time.clock()
-    resolve('ZTB.scd','AccessPoint','IED')
+    resolve('./scd/HSB.scd','AccessPoint','IED')
     end = time.clock()
 
     print("程序总运行时间：",end-start,"s")
