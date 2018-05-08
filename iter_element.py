61ca33e1c748911b8581418caf9b3dfb50dbe1f2
AcerWang <wxy910101@gmail.com>
2018-05-08 18:46:24 +0800

    Add files via upload

--- iter_element.py
+++ iter_element.py
@@ -3,44 +3,59 @@ from db import DataBase
 import xml.etree.ElementTree as ET
 import time
 
-def resolve(scd_file,element,element_parent):
+def resolve(scd_file,element,*element_parents):
     '''
         resolve IED from scd file,
         write IED data into database.
     '''
     iterObj = ET.iterparse(scd_file,events=('start','end'))
-
+    
     parent_no, element_no = 0, 0
     data = []
-    value = None
+    value, target, parent = None, None, None
+    tb_name, tb_desc, fields = None, None, None
 
+    if '/' in element:
+        element = element.split('/')
+        target = element[0]
+    else:
+        target = element
+        element = [element,]
+    
     db = DataBase()
     
-    tb_desc = db.tb_desc('select * from %s limit 1'%element)
-    print(tb_desc)
-    fields = ','.join(['?' for e in tb_desc])
-    insert_sql = "insert into %s values(%s)"%(element,fields)
+    try:
+        tb_desc = db.tb_desc('select * from %s limit 1'%target)
+        print(tb_desc)
+        fields = ','.join(['?' for e in tb_desc])
+        tb_name = target
+    except Exception as e:
+        # for LN0
+        tb_desc = db.tb_desc('select * from %s limit 1'%target[:2])
+        print(tb_desc)
+        fields = ','.join(['?' for e in tb_desc])
+        tb_name = target[:2]
+
+    insert_sql = "insert into %s values(%s)"%(tb_name,fields)
     
     ns = '{http://www.iec.ch/61850/2003/SCL}'
-
+    element = [ ns+e for e in element ]
+    element_parents = [ ns+e for e in element_parents ]
+    parent = element_parents[0]
     in_Parent = False
+    
     for event,ele in iterObj:
         if event == 'start':
-            if ele.tag == ns+element_parent:
+            if ele.tag in element_parents:
+                parent = ele.tag
                 in_Parent = True
                 parent_no += 1
                 continue
 
         elif event == 'end':
             
-            # Val for DAI
-            if ele.tag == ns+'Val':
-                value = ele.text
-                ele.clear()
-                continue
-
             # if end of parent element, clean it, save memory
-            if ele.tag == ns+element_parent:
+            if ele.tag == parent:
                 ele.clear()
                 in_Parent = False
                 continue
@@ -49,21 +64,32 @@ def resolve(scd_file,element,element_parent):
                 ele.clear()
                 continue
             
-            # if not the target element, clean it, save memory
-            if ele.tag != ns+element:
+            # Val for DAI
+            if ele.tag == ns+'Val':
+                value = ele.text
                 ele.clear()
                 continue
             
+            # if not the target element, clean it, save memory
+            if ele.tag not in element:
+                ele.clear()
+                continue
+            
+            # if is target, set `target`= ele.tag
+            target = ele.tag
+
             # if is target element, retrive data
-            for e in ele.iter(ns+element):
+            for e in ele.iter(target):
+                target = target[34:]
+                
                 attrs = []
                 element_no += 1
                 for field in tb_desc:
-                    # doi for ln ln0 use the second to compare
-                    if field.startswith(str.lower(element_parent)) or field.startswith(str.lower(element_parent[:2])):
-                        attrs.append(element_parent+"_"+str(parent_no))
-                    elif field.startswith(str.lower(element)):
-                        attrs.append(element+"_"+str(element_no))
+                    if field == str.lower(parent[34:]) or field == str.lower(parent[34:-1]):
+                        attrs.append(parent[34:]+"_"+str(parent_no))
+                    # in case of that doi for ln, ln0 
+                    elif field == str.lower(target) or field == str.lower(target[:-1]):
+                        attrs.append(target+"_"+str(element_no))
                     else:
                         if field == 'val':
                             attrs.append(value)
