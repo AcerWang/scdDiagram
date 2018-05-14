dbf9ad5565e05356daef8e81f3f8d8ccb6938b3a
AcerWang <wxy910101@gmail.com>
2018-05-14 20:04:41 +0800

    Update Ext_Ref.sql

--- Ext_Ref.sql
+++ Ext_Ref.sql
@@ -1,10 +1,9 @@
--- SELECT ied, AccessPoint, LD_id from IEDTree where ied like 'M%L%' and AccessPoint like 'M%';
--- select ExtRef.* from LN  INNER JOIN ExtRef on LN.id = ExtRef.ln0_id where LN.ldevice_id=38 limit 50 OFFSET 1;
--- select LN.desc,LN.id from LN where LN.ldevice_id=38 and LN.lnClass='TVTR' and LN.inst<=6;
-
 select * from [ExtRef] where [ln0_id] in(
     select id from [LN] where [ldevice_id] in ( SELECT 
          [LD_id]
     FROM   [IEDTree]
     WHERE  [ied] LIKE 'M%L%' AND [AccessPoint] LIKE 'M%' )
-  and lnClass='LLN0' )
+  and lnClass='LLN0' );
+
+select LN.id,IEDTree.IED from LN INNER JOIN IEDTree on LN.ldevice_id=IEDTree.LD_id where LN.ldevice_id in ( 
+SELECT IEDTree.LD_id FROM IEDTree  WHERE  IEDTree.IED LIKE 'M%L%' AND IEDTree.AccessPoint LIKE 'M%' ) and LN.lnClass='LLN0'
