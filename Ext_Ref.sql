6bb1d98d90793a899f1542942d8ff439073810dc
AcerWang <wxy910101@gmail.com>
2018-05-14 21:45:21 +0800

    Update Ext_Ref.sql

--- Ext_Ref.sql
+++ Ext_Ref.sql
@@ -1,11 +1,4 @@
-select * from [ExtRef] where [ln0_id] in(
-    select id from [LN] where [ldevice_id] in ( SELECT 
-         [LD_id]
-    FROM   [IEDTree]
-    WHERE  [ied] LIKE 'M%L%' AND [AccessPoint] LIKE 'M%' )
-  and lnClass='LLN0' ) and lnClass!='LLN0';
+select LN.id,IEDTree.IED,ExtRef.iedName,ExtRef.lnClass,ExtRef.lnInst,ExtRef.ldInst from (LN,ExtRef) INNER JOIN IEDTree on LN.ldevice_id=IEDTree.LD_id where LN.ldevice_id in ( 
+SELECT IEDTree.LD_id FROM IEDTree  WHERE  IEDTree.IED LIKE 'M%L%' AND IEDTree.AccessPoint LIKE 'M%' ) and LN.lnClass='LLN0' and LN.id=ExtRef.ln0_id and ExtRef.lnClass!='LLN0';
 
-select LN.id,IEDTree.IED from LN INNER JOIN IEDTree on LN.ldevice_id=IEDTree.LD_id where LN.ldevice_id in ( 
-SELECT IEDTree.LD_id FROM IEDTree  WHERE  IEDTree.IED LIKE 'M%L%' AND IEDTree.AccessPoint LIKE 'M%' ) and LN.lnClass='LLN0'
-
-select LN.* from LN INNER JOIN IEDTree on LN.ldevice_id=IEDTree.LD_id where LN.lnClass=? and IEDTree.IED = ? and IEDTree.LDevice =? and LN.inst<=?
+select LN.* from LN INNER JOIN IEDTree on LN.ldevice_id=IEDTree.LD_id where LN.lnClass='TVTR' and IEDTree.IED ='MM1101A'  and IEDTree.LDevice ='MUSV01' and LN.inst in (1)
