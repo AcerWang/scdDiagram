63e95e10e5baeb2c324b2da9b324c86d7fb0e32c
AcerWang <wxy910101@gmail.com>
2018-05-15 17:11:36 +0800

    Update ext-ref.sql

--- res/ext-ref.sql
+++ res/ext-ref.sql
@@ -1,8 +1,7 @@
-select LN.id,IEDTree.IED,ExtRef.iedName,ExtRef.lnClass,ExtRef.lnInst,ExtRef.ldInst from (LN,ExtRef) 
+-- CREATE TABLE tmp as SELECT IEDTree.IED as Line,ExtRef.iedName as Ref_To,ExtRef.lnClass,ExtRef.lnInst,ExtRef.ldInst,ExtRef.prefix ,ExtRef.doName from LN,ExtRef
 INNER JOIN IEDTree on LN.ldevice_id=IEDTree.LD_id 
-where LN.ldevice_id in (SELECT IEDTree.LD_id FROM IEDTree  WHERE  IEDTree.IED LIKE 'M%L%' AND IEDTree.AccessPoint LIKE 'M%' )
-and LN.lnClass='LLN0' and LN.id=ExtRef.ln0_id and ExtRef.lnClass!='LLN0';
-
-select LN.* from LN 
-INNER JOIN IEDTree on LN.ldevice_id=IEDTree.LD_id 
-where LN.lnClass='TVTR' and IEDTree.IED ='MM1101A' and IEDTree.LDevice ='MUSV01' and LN.inst in (1)
+where LN.ldevice_id in ( SELECT IEDTree.LD_id FROM IEDTree  WHERE  IEDTree.IED LIKE 'M%L%' AND IEDTree.AccessPoint LIKE 'M%' )
+and LN.lnClass='LLN0'	and LN.id=ExtRef.ln0_id and ExtRef.lnClass!='LLN0';
+ 
+select DISTINCT tmp.Line,tmp.Ref_To,LN.desc from tmp INNER JOIN LN on LN.inst=tmp.lnInst and LN.lnClass=tmp.lnClass 
+where LN.ldevice_id = (select IEDTree.LD_id FROM IEDTree where IEDTree.IED=tmp.Ref_To and IEDTree.LDevice=tmp.ldInst);
