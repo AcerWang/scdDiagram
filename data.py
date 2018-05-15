34019bb7790394b092422d19221ac847c1578477
AcerWang <wxy910101@gmail.com>
2018-05-15 19:04:51 +0800

    Add files via upload

--- data.py
+++ data.py
@@ -1,7 +1,19 @@
 import db
 import re
 import time
-from conv2json import conv2json
+import json
+
+def conv2json(t=None,v=None,b=None,l=None,l_b=None):
+    '''
+    convert data to json format 
+    # t = ['1#','2#']
+    # v = ['110','220','35']
+    # b = ['1101','1102','2201','2202','3501','3502']
+    # l = {'1101':'110#1','1102':'110#2','1103':'110#3','2201':'220#1','2202':'220#2','2203':'220#3','3501':'35#1','3502':'35#2','3503':'35#3'}
+	'''
+    m = {'Trans':t,'Voltage':v,'Bus':b,'Line':l,"Line-Bus":l_b}
+    j_data = json.dumps(m,ensure_ascii=False)
+    return j_data
 
 def getTransformers(db):
     '''
@@ -100,7 +112,36 @@ def getMU(db):
     sql = 'select name,desc from ied where desc like "%合%" and name like "%M%L%"'
     db.select(sql)
 
-
+def which(db):
+    '''
+        获得线路所在母线
+    '''
+    sql = """select DISTINCT tmp.Line,tmp.Ref_To,LN.desc from tmp
+            INNER JOIN LN on LN.inst=tmp.lnInst and LN.lnClass=tmp.lnClass 
+            where LN.ldevice_id = (select IEDTree.LD_id FROM IEDTree where IEDTree.IED=tmp.Ref_To and IEDTree.LDevice=tmp.ldInst)
+        """
+    
+    res = db.select(sql)
+    line_bus = {}
+    desc = list()
+    p = re.compile(r'(\d+)|(I)|(II)|(III)|(IV)|(V)|(VI)|(VII)|(VIII)')
+    for i in res:
+            
+        key = p.search(i[0]).group()
+        value = p.search(i[2]).group()
+        if not i[1].startswith('M'):
+            line_bus[key] = None
+            continue
+        if key in line_bus:
+            if value in desc:
+                continue
+            line_bus[key].append(value)
+        else:
+            desc = list()
+            line_bus[key] = desc
+        
+    #print(line_bus)
+    return line_bus
     
 if __name__=='__main__':
     
@@ -111,10 +152,12 @@ if __name__=='__main__':
     v = getVolts(database)
     b = getBuses(database)
     l = getLines(database)
+    l_b = which(database)
 
-    jsonstr = conv2json(t,v,b,l)
+    jsonstr = conv2json(t,v,b,l,l_b)
+
     print(jsonstr)
-    with open('data.json','w') as f:
+    with open('res/data.json','w') as f:
         f.write(jsonstr)
     
     database.close_connection()
---
+++ scd_struct.py
@@ -0,0 +1,53 @@
+import json
+import xml.etree.ElementTree as ET
+import db
+import time,sys
+
+def resolve_struct(file_name):
+    iterObj = ET.iterparse(file_name,events=('start','end'))
+
+    no_ied, no_ap, no_services, no_server, no_ld = 0, 0, 0, 0, 0
+    data = []
+    ied_name, ap_name, ldInst = None, None, None
+    sql = 'INSERT INTO IEDTree VALUES (?,?,?,?,?,?,?)'
+    ns = '{http://www.iec.ch/61850/2003/SCL}'
+
+    for event, ele in iterObj:
+        if event == 'start':
+            if ele.tag == ns+'DataTypeTemplates':
+                break
+            if ele.tag == ns+'IED':
+                no_ied += 1
+                ied_name = ele.get('name')
+                continue
+            
+            if ele.tag == ns+'Services':
+                no_services += 1
+                continue
+            if ele.tag == ns+'AccessPoint':
+                no_ap += 1
+                ap_name = ele.get('name')
+                continue
+                
+            if ele.tag == ns+'Server':
+                no_server += 1
+                continue
+            if ele.tag == ns+'LDevice':
+                no_ld += 1
+                ldInst = ele.get('inst')
+                data.append((ied_name, no_services, ap_name, no_ap, no_server, ldInst, no_ld))
+                continue
+        elif event == 'end':
+            ele.clear()
+            continue
+
+    database = db.DataBase()
+    database.insert(sql,data)
+    database.close_connection()
+    print('total records:',no_ld)
+
+if __name__ == '__main__':
+    start = time.clock()
+    resolve_struct('scd/ZTB.scd')
+    end = time.clock()
+    print('total time:',end-start,'s')
