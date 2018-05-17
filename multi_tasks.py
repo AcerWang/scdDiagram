7b5604ff00547b796dee3c0858a45e5cf7a6cf3c
AcerWang <wxy910101@gmail.com>
2018-05-17 21:42:56 +0800

    Update multi_tasks.py

--- multi_tasks.py
+++ multi_tasks.py
@@ -1,34 +1,29 @@
-from iter_ele import resolve
+from scd_element import resolve
 from scd_struct import resolve_struct
 
-from threading import Thread
+from multiprocessing.pool import Pool
 import time
 
 
 def multi_task(file):
     
-    task1 = Thread(target=resolve,args=(file,'IED'))
-    task2 = Thread(target=resolve,args=(file,'AccessPoint'))
-    task3 = Thread(target=resolve,args=(file,'LDevice'))
-    task4 = Thread(target=resolve,args=(file,'DAI','DOI'))
-    task5 = Thread(target=resolve_struct,args=(file,))
+    pool = Pool(4)
 
-    task1.start()
-    task2.start()
-    task3.start()
-    task4.start()
-    task5.start()
+    args = [(file,'IED'),(file,'AccessPoint'),(file,'LDevice'),(file,'DAI','DOI'),(file,'LN','LDevice')]
+    
+    for arg in args:
+        pool.apply_async(resolve,args=arg)
+    
+    pool.apply_async(resolve_struct,args=(file,))
+    
+    pool.close()
 
-    task1.join()
-    task2.join()
-    task3.join()
-    task4.join()
-    task5.join()
+    pool.join()
 
     print('done!')
 
 if __name__=='__main__':
     start = time.clock()
-    multi_task('HS220.scd')
+    multi_task('scd/ZTB.scd')
     end = time.clock()
     print('total time:',end-start,'s')
