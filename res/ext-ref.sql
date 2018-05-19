748aceb03514cc1f4d19010c6aaca0c88435e77f
AcerWang <wxy910101@gmail.com>
2018-05-19 21:58:23 +0800

    Update ext-ref.sql

--- res/ext-ref.sql
+++ res/ext-ref.sql
@@ -4,4 +4,4 @@
 -- and LN.lnClass='LLN0'	and LN.id=ExtRef.ln0_id and ExtRef.lnClass!='LLN0';
  
 select DISTINCT tmp.Line,tmp.Ref_To,LN.desc from tmp INNER JOIN LN on LN.inst=tmp.lnInst and LN.lnClass=tmp.lnClass 
-where LN.ldevice_id = (select IEDTree.LD_id FROM IEDTree where IEDTree.IED=tmp.Ref_To and IEDTree.LDevice=tmp.ldInst);
+where LN.ldevice_id = (select IEDTree.LD_id FROM IEDTree where IEDTree.IED=tmp.Ref_To and IEDTree.LDevice=tmp.ldInst) and tmp.Tef_To like 'M%';
