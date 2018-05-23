c7816e3ca4567959a968531ee872cd1eff0cef12
AcerWang <wxy910101@gmail.com>
2018-05-23 11:29:56 +0800

    Add files via upload

--- Draw.py
+++ Draw.py
@@ -50,8 +50,12 @@ class Drawer(object):
         '''
             画母线
         '''
-        max_volt = sorted(self.buses.keys())[-1]
+        volt = sorted(self.buses.keys())
+        max_volt = volt[-1]
+        mid_volt = volt[-2]
+
         if max_volt == 500 or max_volt == 750:
+            # 3/2接线
             x1, x2, y1 = 50, 1050, 200
 
             for i in [1,2]:
@@ -63,15 +67,55 @@ class Drawer(object):
                 self.svg.append(ele)
                 self.svg.append(txt)
                 y1 += 150
+                pass
         else:
-            pass
+            # 普通接线，单母线
+            if max_volt not in bus_relation:
+                ele = Element('line')
+                ele.attrib = {'id':str(max_volt)+'kV_I', 'x1':'50', 'y1':'350', 'x2':'1050', 'y2':'350','stroke':'red', 'stroke-width':'5'}
+                txt = Element('text')
+                txt.attrib =  {'dy':"0", 'stroke':"black", 'stroke-width':"0.5", 'x':'30' ,'y':'355'}
+                txt.text = 'I'
+                self.svg.append(ele)
+                self.svg.append(txt)
+                pass
+            else:
+                if '母联' in bus_relation[max_volt]:
+                    if bus_relation[max_volt]['母联'] is None or len(self.buses[max_volt])==2:
+                        # 直接并联两条母线
+                        x1, x2, y1 = 50, 1050, 350
+                        for i in self.buses:
+                            ele = Element('line')
+                            ele.attrib = {'id':str(max_volt)+'kV_'+Data_process.char_index[i-1], 'x1':str(x1), 'y1':str(y1), 'x2':str(x2), 'y2':str(y1),'stroke':'red', 'stroke-width':'5'}
+                            txt = Element('text')
+                            txt.attrib =  {'dy':"0", 'stroke':"black", 'stroke-width':"0.5", 'x':str(x1-20) ,'y':str(y1+5)}
+                            txt.text = Data_process.char_index[i-1]
+                            self.svg.append(ele)
+                            self.svg.append(txt)
+                            y1 -= 50
+                    else:
+                        import copy
+                        tmp = []
+                        bus = copy.copy(self.buses[max_volt])
+
+                        for i in bus_relation[max_volt]['母联']:
+                            tmp += i
+                        counter = Counter(tmp)
+                        most = counter.most_common()
+                        for i in most:
+                            if i[1]>1:
+                                
+                        pass
+        if mid_volt<100:
+            return
+
     def draw_line(self,line):
         pass
 
 if __name__ == '__main__':
 
-    db = DBHelper.DataBase()
 
+    db = DBHelper.DataBase()
     # 获得主变信息
     trans = Data_process.getTransformers(db)
     # 获得母线信息
@@ -80,11 +124,12 @@ if __name__ == '__main__':
     # lines, line_bus = Data_process.getLineBus(db)
     # 获得母线连接关系
     bus_relation = Data_process.getBusRelationship(db)
-
     db.close_connection()
 
-    etree = ET.parse("test.html")
-    
-    drawer = Drawer(et=etree,trans=trans,buses=buses)
 
-    drawer.draw()
+    
+    # etree = ET.parse("test.html")
+    # drawer = Drawer(et=etree,trans=trans,buses=buses)
+    # drawer.draw()
+    print(buses)
+    print(bus_relation)
