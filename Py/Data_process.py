464f796feb569c9cb6c6fc32baba7bf823ad5cad
AcerWang <wxy910101@gmail.com>
2019-03-06 14:15:09 +0800

    Update Data_process.py

--- Py/Data_process.py
+++ Py/Data_process.py
@@ -1,7 +1,8 @@
-from DBHelper import DataBase
-
 import re
 import json
+
+from DBHelper import DataBase
+
 
 dig_index = ['1','2','3','4','5','6','7','8','9']
 char_index = ['I','II','III','IV','V','VI','VII','VIII','IX']
